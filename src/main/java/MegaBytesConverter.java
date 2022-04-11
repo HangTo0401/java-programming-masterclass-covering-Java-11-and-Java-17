c2a0c752e944d30c306225cbaf7207b0b42bb43c
HangTo <tominhhang1996@gmail.com>
2022-04-11 11:47:26 +0700

    Implement Coding exercise 3

--- src/main/java/MegaBytesConverter.java
+++ src/main/java/MegaBytesConverter.java
@@ -1,5 +1,5 @@
 /**
- * Write a method called printMegaBytesAndKiloBytes that has 1 parameter of type int with the name kiloBytes.
+ * Coding exercise 2: Write a method called printMegaBytesAndKiloBytes that has 1 parameter of type int with the name kiloBytes.
  *
  * The method should not return anything (void) and it needs to calculate the megabytes and remaining kilobytes from the kilobytes parameter.
  * Then it needs to print a message in the format "XX KB = YY MB and ZZ KB".
